bd985b2355e559eed11766d0594e44cea834e2a4
David Gengenbach <davidgengenbach@gmx.de>
2016-11-09 11:21:48 +0100

    :)

---
+++ dataset/extract-cols.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+extract_columns() {
+    echo -e "\tExtracting column: $1\t// $2 // $3"
+    local COLUMNS=$1; local IN=$2; local OUT=$3;
+    csvtool col $COLUMNS $IN > $OUT
+}
+
+extract_ann_cols() {
+    local IN=$1
+    echo -e "\n\nextract_ann_cols: $IN"
+
+    extract_columns 1,3 $IN/labels.csv      $IN/labels_1_3.csv || exit
+    extract_columns 1   $IN/labels_1_3.csv  $IN/labels_1.csv || exit
+    extract_columns 2   $IN/labels_1_3.csv  $IN/labels_3.csv || exit
+    extract_columns 4   $IN/labels.csv      $IN/labels_4.csv || exit
+
+    echo -e "\n\tStarting to count"
+    ./wordcount.py $IN/labels_1.csv > /dev/null|| exit
+    ./wordcount.py $IN/labels_3.csv > /dev/null || exit
+    ./wordcount.py $IN/labels_4.csv > /dev/null || exit
+
+    echo -e "\n\tLabels per image"
+    ./labels-per-image.sh $IN
+}
+
+extract_img_cols() {
+    local IN=$1
+    echo -e "extract_img_cols: $IN"
+    extract_columns 1,3,4   $IN/labels.csv          $IN/labels_1_3_4.csv || exit
+    extract_columns 1,2     $IN/labels_1_3_4.csv    $IN/labels_1_3.csv || exit
+    extract_columns 9       $IN/labels.csv          $IN/labels_9.csv || exit
+}
+
+extract_ann_cols "download/human_ann_2016_08/validation"
+extract_ann_cols "download/machine_ann_2016_08/validation"
+extract_ann_cols "download/machine_ann_2016_08/train"
+
+echo -e "labels-mispredicted"
+./labels-mispredicted.sh
+
+extract_img_cols "download/images_2016_08/validation"
+extract_img_cols "download/images_2016_08/train"
