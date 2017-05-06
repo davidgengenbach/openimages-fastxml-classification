a46e89df94ccfcd856554e4e308cdc617cb74c8f
David Gengenbach <davidgengenbach@gmx.de>
2017-05-06 20:52:58 +0200

    :)

--- tmp/feature_extraction/scripts/split_dataset.sh
+++ tmp/feature_extraction/scripts/split_dataset.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
 INPUT="$1"
-FOLDS="$2"
+OUT_FOLDER="$2"
+FOLDS="$3"
 
-if [ -z "$INPUT" ] || [ -z "$FOLDS" ]; then
-    echo "Usage: $0 input.txt folds"
+if [ -z "$INPUT" ] || [ -z "$FOLDS" ] || [ -z "$OUT_FOLDER" ]; then
+    echo "Usage: $0 input.txt out_folder/ num_folds"
     exit 1
 fi
 
 LINE_COUNT=$(cat $INPUT | wc -l)
 ELEMENTS_PER_FOLD=$((${LINE_COUNT} / $FOLDS))
 
-
 echo "LineCount: $LINE_COUNT"
 
-for i in $(seq 1 $(($FOLDS - 1))); do
-    start=$(($i * $ELEMENTS_PER_FOLD))
-    end=$(($start + $ELEMENTS_PER_FOLD))
-    echo $start / $end
+for i in $(seq 0 $(($FOLDS - 1))); do
+    start=$(($i * $ELEMENTS_PER_FOLD + 1))
+    end=$(($start + $ELEMENTS_PER_FOLD - 1))
+    if [ $i == $((FOLDS - 1)) ]; then
+        end=$LINE_COUNT
+    fi
+    sed "${start},\$!d;${end}q" $INPUT > $OUT_FOLDER/$INPUT.$i.txt
 done
