526bbefcb6b74dc03ada967f138b6f26d0c53577
David Gengenbach <davidgengenbach@gmx.de>
2017-05-06 23:28:59 +0200

    :)

--- tmp/bootstrap/download-model.sh
+++ tmp/bootstrap/download-model.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# VGG16 https://mega.nz/#!YU1FWJrA!O1ywiCS2IiOlUCtCpI6HTJOMrneN-Qdv3ywQP5poecM
-# VGG19 https://mega.nz/#!xZ8glS6J!MAnE91ND_WyfZ_8mvkuSa2YcA7q-1ehfSm-Q1fxOvvs
 
 url=$1
 enc_file=$2.enc
