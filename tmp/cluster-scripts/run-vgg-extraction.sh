447a1e35849c24c7c908c7eb64fce52171d714b6
David Gengenbach <davidgengenbach@gmx.de>
2017-05-06 23:34:39 +0200

    :)

---
+++ tmp/cluster-scripts/run-vgg-extraction.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+TENSORFLOW_VGG_PATH="/nfs/cluster_files/dgengenbach/ml-praktikum/vgg/tensorflow-vgg"
+DATA_PATH="/nfs/cluster_files/dgengenbach/ml-praktikum/data"
+
+FEATURE_TO_EXTRACT=$1
+
+if [ -z "$FEATURE_TO_EXTRACT" ]; then
+    echo "Usage: $0 feature_layer_to_extract"
+    exit 1
+fi
+
+cd $TENSORFLOW_VGG_PATH
+
+python3 test_vgg16.py \
+    --images-list-file $DATA_PATH/images.txt \
+    --images-path $DATA_PATH/val_imgs/ \
+    --done-file $DATA_PATH/done.$FEATURE_TO_EXTRACT.txt \
+    --features-file $DATA_PATH/features.$FEATURE_TO_EXTRACT.txt \
+    --batch-size 16 \
+    --extract-layers $FEATURE_TO_EXTRACT
