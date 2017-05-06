43edbac21cbab6f75c54f903f335f82606e3a238
David Gengenbach <davidgengenbach@gmx.de>
2017-05-06 22:53:21 +0200

    :)

--- tmp/bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-set -x
-
-docker build -t caffe-tf:cpu docker
--- tmp/bootstrap/download-openimages.sh
+++ tmp/bootstrap/download-openimages.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 wget --continue -O val.tar.gz https://doc-0s-90-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/82cok8updk8eqj91a8tr4vbvpgl9rs40/1484251200000/15382047144543476470/*/0B3TtLmV-bYCebDRjNEd5aXhKdVk?e=download
