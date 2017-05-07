d36ce063eb9044181a522b8ed544a8f1e65808e7
David Gengenbach <davidgengenbach@gmx.de>
2017-05-07 13:40:49 +0200

    :)

--- tmp/feature_extraction/remove_duplicates.sh
+++ tmp/feature_extraction/remove_duplicates.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 IN=$1
+IN_IMAGES=$2
 
-if [ -z "$IN" ]; then
-    echo "Usage: $0 features.ids.txt"
+
+if [ -z "$IN" ] || \
+   [ -z "$IN_IMAGES" ]; then
+    echo "Usage: $0 features.ids.txt features.txt"
     exit 1
 fi
 
 for id in $(sort $IN | uniq -c | sort -r | grep "2 " | cut -d " " -f 8); do
-    echo "Yes $id"
+    cat $IN_IMAGES | grep "$id"
 done
