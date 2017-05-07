2552a1a469ddbbab9603942a15fdfba103c65a3b
David Gengenbach <davidgengenbach@gmx.de>
2017-05-07 13:47:02 +0200

    :)

--- tmp/feature_extraction/remove_duplicates.sh
+++ tmp/feature_extraction/remove_duplicates.sh
@@ -11,7 +11,8 @@ if [ -z "$IN" ] || \
 fi
 
 for id in $(sort $IN | uniq -c | sort -r | grep "2 " | cut -d " " -f 8); do
-        feature=$(cat $IN_IMAGES | grep -n "$id" | head -n1)
+        feature=$(cat $IN_IMAGES | grep -n -m1 "$id")
+        echo $feature
         #echo $id, $(echo $feature | cut -d , -f 1)
         #sed -i "/$id,/d" $IN_IMAGES
         #echo $feature >> $IN_IMAGES
