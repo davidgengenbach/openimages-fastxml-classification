be6e2e5e1916f8183f8b794e3d19e97d0c0f6a35
David Gengenbach <davidgengenbach@gmx.de>
2017-05-07 12:39:45 +0200

    :)

--- tmp/feature_extraction/extract_feature_ids.sh
+++ tmp/feature_extraction/extract_feature_ids.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env sh
 
 TMP=$(mktemp)
-OUT=features.ids.txt
+OUT=$1
+shift
 IN=$@
 
-if [ -z "$IN" ]; then
-    echo "Usage: $0 features.fc8.*.txt"
+if [ -z "$IN" ] || \
+   [ -z "$OUT" ]; then
+    echo "Usage: $0 feature.ids.txt features.fc8.*.txt"
     exit 1
 fi
 
 echo > $TMP
 
 for file in $IN; do
-	if [ "$file" != "features.ids.txt" ]; then
+	if [ "$file" != "$OUT" ]; then
 		cat $file | cut -d , -f 1 | grep -v "^$" >> $TMP
 	fi
 done
