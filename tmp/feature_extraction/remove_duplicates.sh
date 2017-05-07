93a143726a35dc7c4bf6572e04efdf0079d8d2cd
David Gengenbach <davidgengenbach@gmx.de>
2017-05-07 13:48:57 +0200

    :)

--- tmp/feature_extraction/remove_duplicates.sh
+++ tmp/feature_extraction/remove_duplicates.sh
@@ -12,7 +12,10 @@ fi
 
 for id in $(sort $IN | uniq -c | sort -r | grep "2 " | cut -d " " -f 8); do
         feature=$(cat $IN_IMAGES | grep -n -m1 "$id")
-        echo $feature
+        line=$(echo $feature | cut -d ":" -f 1)
+        line_plus_one=$(($line + 1))
+        echo $line $line_plus_one
+        #sed -i.bak -e '5,10d;12d' file
         #echo $id, $(echo $feature | cut -d , -f 1)
         #sed -i "/$id,/d" $IN_IMAGES
         #echo $feature >> $IN_IMAGES
