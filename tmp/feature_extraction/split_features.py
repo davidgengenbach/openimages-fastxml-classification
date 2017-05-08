2ca81e43ab0d0991bec640112611b9d7c26b3099
David Gengenbach <davidgengenbach@gmx.de>
2017-05-08 15:24:27 +0200

    :)

--- tmp/feature_extraction/split_features.py
+++ tmp/feature_extraction/split_features.py
@@ -6,9 +6,8 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser(description='Split features')
     parser.add_argument('--in-file', type=str, default='data/features.fc7.fc8.small.txt')
-    parser.add_argument('--ignore-header', default=True, action='store_false')
-    parser.add_argument('--out-1-file', type=str, default='data/features.fc7.txt')
-    parser.add_argument('--out-2-file', type=str, default='data/features.fc8.txt')
+    parser.add_argument('--split-points', type=str, default='4096,8192')
+    parser.add_argument('--out_folder', type=str, default='data/splits')
     args = parser.parse_args()
     return args
 
@@ -16,31 +15,25 @@ def get_args():
 def main():
     args = get_args()
     in_file = open(args.in_file)
-    if args.ignore_header:
-        header = next(in_file)
 
-    out_file_1 = open(args.out_1_file, 'w')
-    out_file_2 = open(args.out_2_file, 'w')
-
-    out_file_1.write(header)
-    out_file_2.write(header)
-
-    count = 4096
+    split_points = [int(x) for x in args.split_points.split(',')]
+    files = [open('{}/split.{}.txt'.format(args.out_folder, x), 'w') for x in range(len(split_points) + 1)]
+    comma_count = None
     for line in in_file:
         if line.strip() == '':
             continue
-        comma_count = line.count(',')
-        parts = line.split(',', count + 1)
-        assert(len(parts) == count + 2)
+        if not comma_count:
+            comma_count = line.count(',')
+            split_points.append(comma_count)
+        parts = line.split(',')
         iid = parts[0]
-        part_1 = parts[1:-1]
-        assert(len(part_1) == count)
-        assert(parts[-1].count(',') == 1000 - 1)
-        out_file_1.write(",".join([iid] + part_1) + '\n')
-        out_file_2.write(iid + ',' + parts[-1] + '\n')
+        for idx, split_point in enumerate(split_points):
+            last = 0 if idx == 0 else split_points[idx - 1]
+            part = parts[last + 1:split_point + 1]
+            files[idx].write(','.join([iid] + part))
 
-    out_file_1.close()
-    out_file_2.close()
+    for file in files:
+        file.close()
 
 if __name__ == '__main__':
     main()
