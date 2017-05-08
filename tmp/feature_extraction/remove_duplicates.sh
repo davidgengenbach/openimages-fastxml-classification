38626edf7168fe27d9b27b98d123ae025057b4b1
David Gengenbach <davidgengenbach@gmx.de>
2017-05-08 14:30:07 +0200

    :)

--- tmp/feature_extraction/remove_duplicates.sh
+++ tmp/feature_extraction/remove_duplicates.sh
@@ -9,14 +9,17 @@ if [ -z "$IN" ] || \
     echo "Usage: $0 features.ids.txt features.txt"
     exit 1
 fi
-
+lines=""
 for id in $(sort $IN | uniq -c | sort -r | grep -v " 1 " | cut -d " " -f 8); do
         feature=$(cat $IN_IMAGES | grep -n -m1 "$id")
         line=$(echo $feature | cut -d ":" -f 1)
         line_plus_one=$(($line + 1))
+        lines="$lines;${line},${line_plus_one}d"
         #echo $line $line_plus_one
-        sed -i -e "${line},${line_plus_one}d" $IN_IMAGES
+        #sed -i -e "${line},${line_plus_one}d" $IN_IMAGES
         #echo $id, $(echo $feature | cut -d , -f 1)
         #sed -i "/$id,/d" $IN_IMAGES
         #echo $feature >> $IN_IMAGES
 done
+
+echo $lines
